f857c5b016f1003b510ed0746cf1ec048335c57c
Vikash Gandham <142613443+VikashGandham@users.noreply.github.com>
2025-11-22 09:36:07 +0530

    Update app.java

--- app.java
+++ app.java
@@ -3,3 +3,9 @@ addin java code here
 added extra line after configuring jenkins
 
 this line is added to check if the webhook is workin gor not
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
